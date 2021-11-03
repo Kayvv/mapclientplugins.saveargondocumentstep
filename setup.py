0b34cc1541f67b7babb88ef3aef3e157daa0e385
hsorby <h.sorby@auckland.ac.nz>
2021-11-04 01:20:48 +1300

    Use single source version in setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-import os
+import codecs
 import io
+import os
+import re
+
+from setuptools import setup, find_packages
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
+def read(*parts):
+    with codecs.open(os.path.join(SETUP_DIR, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 # List all of your Python package dependencies in the
 # requirements.txt file
@@ -20,22 +35,13 @@ def readfile(filename, split=False):
 readme = readfile("README.rst", split=True)[3:]  # skip title
 # For requirements not hosted on PyPi place listings
 # into the 'requirements.txt' file.
-requires = ['mapclient', 'PySide2']  # minimal requirements listing
+requires = ['PySide2']  # minimal requirements listing
 source_license = readfile("LICENSE")
-
-
-class InstallCommand(install):
-
-    def run(self):
-        install.run(self)
-        # Automatically install requirements from requirements.txt
-        import subprocess
-        subprocess.call(['pip', 'install', '-r', os.path.join(SETUP_DIR, 'requirements.txt')])
 
 
 setup(
     name='mapclientplugins.saveargondocumentstep',
-    version='0.1.0',
+    version=find_version('mapclientplugins', 'saveargondocumentstep', '__init__.py'),
     description='',
     long_description='\n'.join(readme) + source_license,
     long_description_content_type='text/x-rst',
@@ -44,7 +50,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
     ],
-    cmdclass={'install': InstallCommand,},
     author='Hugh Sorby',
     author_email='',
     url='',
